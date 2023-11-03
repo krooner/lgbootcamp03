6a39f3051cac9964d5e41fbd82a21604f322a76a
krooner <krooner.kim@gmail.com>
2023-11-03 04:57:28 +0000

    add main.py

--- project/face_classification/src/main.py
+++ project/face_classification/src/main.py
@@ -1,4 +1,5 @@
 from face_module import recognize_emotion 
-
+from ai_chat import ai_chat
 if __name__ == "__main__":
-    recognize_emotion(detection_duration=10.)
+    # result = recognize_emotion(detection_duration=10.)
+    ai_chat("Angry")
