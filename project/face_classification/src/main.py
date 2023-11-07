8e65021df3891ed992d5b88a9d1fad68924b94d4
krooner <krooner.kim@gmail.com>
2023-11-07 01:00:32 +0000

    Update main.py

--- project/face_classification/src/main.py
+++ project/face_classification/src/main.py
@@ -1,26 +1,31 @@
 from face_module import recognize_emotion 
-from ai_chat import ai_chat
-from ask_music_preference import chat_start
-from music_player import music_player
+from ai_chat import *
+from ask_music_preference import *
+from music_player import *
 from smtp import send_email
 
 if __name__ == "__main__":
-    before_result = recognize_emotion(detection_duration=10.)
+    before_emotion, before_emotion_prob = recognize_emotion(detection_duration=10., before=True)
+
+    if before_emotion == None:
+        print("Failed to detect emotion")
+        exit(0)
 
     # Function 1: Sending Email for light automation
-    send_email(before_result)
+    send_email(before_emotion)
 
     # Function 2: asking answer for music recommendation
-    is_recommended = chat_start(before_result)
+    is_recommended = ask_music_preference(before_emotion)
     if is_recommended: # If True, play music
-        music_player(before_result)
+        music_player(before_emotion)
     else: # If False, start conversation with AI Speaker
-        ai_chat(before_result, turn_count=3)
+        ai_chat(before_emotion, turn_count=3)
 
-    after_result = recognize_emotion(detection_duration=10.)
+    after_emotion, after_emotion_prob = recognize_emotion(detection_duration=10.)
 
+    # TODO: send an email containing emotion statistics
     print(f"""
-        Emotion before: {before_result}
-        Emotion After: {after_result}
+        Emotion before: {before_emotion}
+        Emotion After: {after_emotion}
     """)
 
